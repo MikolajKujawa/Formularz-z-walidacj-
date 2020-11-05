ca3224832f1f389c0cb883923ac08d98390e90f4
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-05 18:58:47 +0100

    Updates

---
+++ precache-manifest.5b5b30b59708139f5bf5ac7551bf13b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f447a152d0d7d0f3b58",
+    "url": "/Formularz-z-walidacja/static/css/main.c4da593e.chunk.css"
+  },
+  {
+    "revision": "7f447a152d0d7d0f3b58",
+    "url": "/Formularz-z-walidacja/static/js/main.f6a86b11.chunk.js"
+  },
+  {
+    "revision": "9993f2b9aac56bb733aa",
+    "url": "/Formularz-z-walidacja/static/js/runtime~main.318cc42d.js"
+  },
+  {
+    "revision": "3c83802b4bb431dfcc4c",
+    "url": "/Formularz-z-walidacja/static/js/2.13cc546b.chunk.js"
+  },
+  {
+    "revision": "85f79b7713d1b610459604186f1190ea",
+    "url": "/Formularz-z-walidacja/index.html"
+  }
+];
